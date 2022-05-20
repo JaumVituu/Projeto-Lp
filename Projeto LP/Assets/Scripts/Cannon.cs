b1287789c700735f95a9e992ebc2b2992c24ce69
JaumVituu <78977800+JaumVituu@users.noreply.github.com>
2022-05-20 11:29:30 -0300

    Inclusão inicial

---
+++ Projeto LP/Assets/Scripts/Cannon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject tiroPrefab;
+    public Vector3 posicaoAtual;
+    float tempoTiro;
+
+    void Start()
+    {
+        tempoTiro = 1f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tempoTiro <1f){
+            tempoTiro += Time.deltaTime;
+        }
+        posicaoAtual = transform.position;
+        DetectPlayer();
+    }
+
+    void DetectPlayer(){
+        RaycastHit hit;
+        Ray ray = new Ray(posicaoAtual,new Vector3(0,0,-1));
+        if (Physics.Raycast(ray,out hit,9f)){
+            Shoot();
+        }
+    }
+    void Shoot(){
+        if (tempoTiro >= 1f){
+            Instantiate(tiroPrefab, posicaoAtual + new Vector3(0,0,-2), Quaternion.identity);
+            Debug.Log("Atirou");
+            tempoTiro = 0f;
+        }        
+    }
+}
---
+++ Projeto LP/Assets/Scripts/Tiro.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiro : MonoBehaviour
+{
+    float tempoRestante;    
+    
+    void Update()
+    {
+        tempoRestante += Time.deltaTime;
+        Movimenta();
+        if(tempoRestante >= 1f){
+            Destroi();
+        }
+    }
+
+    void Movimenta(){
+        transform.Translate(new Vector3(0,0,-0.05f));
+    }
+
+    void Destroi(){
+        Destroy(gameObject);
+    }
+}
